a9cd8ee50ac71b2fb9c493df0dfffc4345234771
Sadik Ali <sadikofficial011@gmail.com>
2024-10-16 15:41:33 +0530

    Add reset button

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const count = document.querySelector('.count')
 const minus = document.querySelector('.minus-btn')
 const plus = document.querySelector('.plus-btn')
 const changeBy = document.querySelector('.changeBy')
+const resetBtn = document.querySelector('.reset-btn')
+
 
 minus.addEventListener('click', ()=>{
     const countValue = parseInt(count.innerText)
@@ -12,4 +14,7 @@ plus.addEventListener('click',()=>{
     const countValue = parseInt(count.innerText)
     const changeByvalue = parseInt(changeBy.value)
     count.innerText = countValue + changeByvalue
+})
+resetBtn.addEventListener('click', ()=>{
+    count.innerText = 0
 })
